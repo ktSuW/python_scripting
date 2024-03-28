487f7f0e845350eabd8dc99b3a676b2ec67ff37b
su <suwindev19@gmail.com>
2024-03-28 07:43:37 -0400

    Refactored mac_changer

--- scripts/mac_changer.py
+++ scripts/mac_changer.py
@@ -3,16 +3,27 @@
 import subprocess
 import optparse
 
-parser = optparse.OptionParser()
-parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
-parser.parse_args()
+def get_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
+    parser.add_option("-m", "--mac", dest="new_mac_address", help="New MAC Address")
+    (options, arguments) = parser.parse_args()
+    if not options.interface:
+        parser.error("[-] Please specify interface. Use --help for more info.")
+    elif not options.new_mac_address:
+        parser.error("[-] Please specify a new mac. Use --help for more info.")
+    return options
 
-interface = input("Interface : ")
-new_mac_address = input("Enter new MAC address : ")
-# subprocess.call(f"ifconfig {interface} down", shell=True)
-# subprocess.call(f"ifconfig {interface} hw ether {new_mac_address}", shell=True)
-# subprocess.call(f"ifconfig {interface} up", shell=True)
+def change_mac(interface, new_mac_address):
+    print(f"[+] Changing MAC address for {interface} to {new_mac_address}")
+    # security risk
+    # subprocess.call(f"ifconfig {interface} down", shell=True)
+    # subprocess.call(f"ifconfig {interface} hw ether {new_mac_address}", shell=True)
+    # subprocess.call(f"ifconfig {interface} up", shell=True)
+    subprocess.call(["ifconfig", interface, "down"])
+    subprocess.call(["ifconfig", interface, "hw" , "ether", new_mac_address])
+    subprocess.call(["ifconfig", interface, "up"])
+    
 
-subprocess.call(["ifconfig", interface, "down"])
-subprocess.call(["ifconfig", interface, "hw" , "ether", new_mac_address])
-subprocess.call(["ifconfig", interface, "up"])
+options = get_arguments()
+change_mac(options.interface, options.new_mac_address)
