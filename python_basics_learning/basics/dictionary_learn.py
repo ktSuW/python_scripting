65e2a12e6e4c081d7471350b077e869be0b8c50a
su <suwindev19@gmail.com>
2024-04-08 08:17:12 -0400

    Added dictionary simple notes

---
+++ python_basics_learning/basics/dictionary_learn.py
@@ -0,0 +1,54 @@
+"""
+
+a
+b
+c
+d
+
+"""
+
+morse_code = {
+    "A" : ". -",
+    "B" : "- . . .",
+    "C" : "- . - .",
+    "D" : "- . .",
+    "E" : ".",
+    "F" : ". . - .",
+    "G" : "- - ."
+}
+print(morse_code)
+
+morse_code3 = {}
+
+morse_code3["A"] = ". -"
+morse_code3["B"] = "- . . ."
+morse_code3["C"] = "- . - ."
+morse_code3["D"] = "- . ."
+morse_code3["E"] = "."
+morse_code3["F"] = ". . - ."
+morse_code3["G"] = "- - ."
+
+print(morse_code3)
+
+
+
+
+# print(morse_code)
+
+x= {"- . - ."} # set
+# print(type(x))
+
+morse_code1 = {
+    "C" : "- . - .",
+    "D" : "- . ."
+}
+morse_code1["A"] = ". -"
+morse_code1["B"] = "- . . ."
+# print(morse_code1)
+
+
+morse_code2 = {
+    "A" : ". -",
+    "B" : "- . . ."
+}
+# print(morse_code2)
---
+++ python_basics_learning/coding_challenge_2.py
@@ -0,0 +1,20 @@
+"""
+Coding Challenge 2: Password Validator
+Scenario: Write a Python program that acts as a password validator. The program should ask the user to input a password and check if it meets the following criteria:
+Minimum length: 8 characters
+At least one uppercase letter (A-Z)
+At least one lowercase letter (a-z)
+At least one digit (0-9)
+At least one special character (~!@#$%^&*()_+-={}|[]\:;'<>,.?/)
+Output:
+If the password meets all criteria, print a message stating "Password is valid."
+If the password fails any criteria, print a message stating "Password is invalid." and list the specific criteria that were not met.
+
+Bonus:
+Implement a password strength meter that assigns a score based on the number of criteria met and the length of the password.
+Allow users to input the minimum password length and allowed special characters.
+"""
+
+        
+        
+        
