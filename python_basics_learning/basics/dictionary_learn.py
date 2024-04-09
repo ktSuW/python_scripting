9a7b849e419e30731328cf3cd4eb965b4283b921
su <suwindev19@gmail.com>
2024-04-09 06:56:20 -0400

    Added dictionary notes

--- python_basics_learning/basics/dictionary_learn.py
+++ python_basics_learning/basics/dictionary_learn.py
@@ -1,12 +1,87 @@
 """
+dic1 = dict()
 
-a
-b
-c
-d
+dict2 = dict((iterable pairs))
 
+dict3 = dict(zip(iterable, iterable))
+
+dict4 = dict(enumerate(iterable))
+
+dict5 = {}
+
+dict6 = {exp : exp for item in iterable}
+
+dict7 = {(exp, exp) for item in iterable}
+
+dict8 = {(exp, exp) for item in zip(iterable, iterable)}
+
+dict9 = {((exp, exp) for item in enumerate(iterable))}
+
+
+['ArithmeticError', 'AssertionError', 'AttributeError', 'BaseException', 'BaseExceptionGroup', 'BlockingIOError', 'BrokenPipeError', 'BufferError', 'BytesWarning', 'ChildProcessError', 'ConnectionAbortedError', 'ConnectionError', 'ConnectionRefusedError', 'ConnectionResetError', 'DeprecationWarning', 'EOFError', 'Ellipsis', 'EncodingWarning', 'EnvironmentError', 'Exception', 'ExceptionGroup', 'False', 'FileExistsError', 'FileNotFoundError', 'FloatingPointError', 'FutureWarning', 'GeneratorExit', 'IOError', 'ImportError', 'ImportWarning', 'IndentationError', 'IndexError', 'InterruptedError', 'IsADirectoryError', 'KeyError', 'KeyboardInterrupt', 'LookupError', 'MemoryError', 'ModuleNotFoundError', 'NameError', 'None', 'NotADirectoryError', 'NotImplemented', 'NotImplementedError', 'OSError', 'OverflowError', 'PendingDeprecationWarning', 'PermissionError', 'ProcessLookupError', 'RecursionError', 'ReferenceError', 'ResourceWarning', 'RuntimeError', 'RuntimeWarning', 'StopAsyncIteration', 'StopIteration', 'SyntaxError', 'SyntaxWarning', 'SystemError', 'SystemExit', 'TabError', 'TimeoutError', 'True', 'TypeError', 'UnboundLocalError', 'UnicodeDecodeError', 'UnicodeEncodeError', 'UnicodeError', 'UnicodeTranslateError', 'UnicodeWarning', 'UserWarning', 'ValueError', 'Warning', 'WindowsError', 'ZeroDivisionError', '__build_class__', '__debug__', '__doc__', '__import__', '__loader__', '__name__', '__package__', '__spec__', 'abs', 'aiter', 'all', 'anext', 'any', 'ascii', 'bin', 'bool', 'breakpoint', 'bytearray', 'bytes', 'callable', 'chr', 'classmethod', 'compile', 'complex', 'copyright', 'credits', 'delattr', 'dict', 'dir', 'divmod', 'enumerate', 'eval', 'exec', 'exit', 'filter', 'float', 'format', 'frozenset', 'getattr', 'globals', 'hasattr', 'hash', 'help', 'hex', 'id', 'input', 'int', 'isinstance', 'issubclass', 'iter', 'len', 'license', 'list', 'locals', 'map', 'max', 'memoryview', 'min', 'next', 'object', 'oct', 'open', 'ord', 'pow', 'print', 'property', 'quit', 'range', 'repr', 'reversed', 'round', 'set', 'setattr', 'slice', 'sorted', 'staticmethod', 'str', 'sum', 'super', 'tuple', 'type', 'vars', 'zip']
 """
+# list1 = {"A", "B", "C", "D"}
+# print(type(list1))
+# list2 = ["apple", "banana", "citrus"]
+# print(type(list2))
+# dict3 = dict(zip(list1, list2))
+# print(dict3)
 
+# set1 = {1,3,5}
+# set2 = "ABC"
+# dict3 = dict(zip(set1, set2))
+# print(dict3)
+# Extra values will be ignored
+# dict3 = dict(zip([1,2,3], (11,22,33,44,55)))
+# print(dict3)
+
+print(dir(__builtins__))
+
+#=================================
+# It can be a pair of any iterable
+# dict2 = dict(((1,"Akai") , (2, "Aoki"), (3, "Midori")))
+# dict2 = dict(([1,1,45], [2,20], [3,56])) #ValueError: dictionary update sequence element #0 has length 3; 2 is required
+# dict2 = dict(('ab', 'cd', 'ef'))
+# list1 = [(1,2), (3,4), (5,6)]
+# dict2 = dict(list1)
+# print(dict2)
+#=======================================
+dict5 = {}
+for x in range(1,6):
+    dict5[x] = 2*x
+
+# print(dict5)
+#============================
+# dict1 = dict()
+# print(type(dict1))
+# dict1["apple"] = "red"
+# dict1["mango"] = "yellow"
+# dict1["grape"] = "purple"
+
+# for x in range(1,6):
+#     dict1[x]= x**2
+# print(dict1)
+
+#=================================
+# dic2 = {
+#     101: "Takana",
+#     102 : "Yamada",
+#     103 : "Sakura"
+# }
+
+# # a = dic2[0] # KeyError
+# # print(a)
+# dic2[104] = "Kentaro"
+# # del dic2[4]
+# for i in dic2:
+#     print(i, dic2[i])
+
+
+# dicionary
+# https://www.youtube.com/watch?v=XYkyKhV5Ifk&list=PLH6mU1kedUy9ScsTt2MCE9k9ung8dy5Yp
+# searching O(1)
+# pair, entry, item
+# Any data type for 
 morse_code = {
     "A" : ". -",
     "B" : "- . . .",
---
+++ python_basics_learning/coding_challenge_3.py
@@ -0,0 +1,53 @@
+"""
+Below are the score values used in Scrabble for each letter.
+
+A:1 point
+B:3 points
+C:3 points
+D:2 points
+E:1 point
+F:4 points
+G:2 points
+H:4 points
+I:1 point
+J:8 points
+K:5 points
+L:1 point
+M:3 points
+N:1 point
+O:1 point
+P:3 points
+Q:10 points
+R:1 point
+S:1 point
+T:1 point
+U:1 point
+V:4 points
+W:4 points
+X:8 points
+Y:4 points
+Z:10 points 
+
+Write a program to prompt the user for a word, then output the score for the word.
+If the user enters 'quit' the program ends.
+Allow the user to enter words in upper or lower case.
+
+Example Output:
+
+	Enter a Word: dog
+
+	WORD: dog -> SCORE: 5
+
+	Enter a Word: Horse
+
+	WORD: Horse -> SCORE: 8
+
+	Enter a Word: Xylophone
+
+	WORD: Xylophone -> SCORE: 24
+
+	Enter a Word: quit
+	
+Hint: read the scores as a multi-line string and use the 'splitlines()' method to get started
+
+"""
