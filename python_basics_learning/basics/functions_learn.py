f6eab489e8c0f6903e4775751e86a956f4dc1214
su <suwindev19@gmail.com>
2024-04-18 22:59:36 +1000

    Added function study notes

--- python_basics_learning/basics/functions_learn.py
+++ python_basics_learning/basics/functions_learn.py
@@ -13,11 +13,40 @@ Order of Python Function Arguments
 - def function(a,b,/,c,d, e=v1, f=v2, *arg, g, h, i=v3, **kwarg)
 - **kwarg arbitary number of keywords arguments
 """
+# Default arguments
+# default should be on the right side and is created only once
+def add(a,b = 0,c=1):
+    return a + b + c
 
-def test_call_by_value_reference(arg):
-    print("ID inside the function: " , id(arg))
-    arg = arg + 1 
-    print("new object after increment", arg, id(arg))
+a1 = add(1,2)
+a2 = add(3,4,5)
+a3 = add(100)
+print(a1, a2, a3)
+
+# Positional vs Keyword arguments
+# def net_salary(basic, allowance, deduction):
+#     net = basic + allowance - deduction
+#     return net 
+# Positional arguments
+# net = net_salary(80,50,20)
+# print(net)\
+# Keyword arguments
+# net = net_salary(30, allowance=45, deduction=10)
+# print(net)
+# def add_nums(a,b,c):
+#     result = a + b + c
+#     print(id(a), id(b), id(c))
+#     return result
+# a,b,c = 3,8,7
+# # call by reference
+# print(id(a), id(b), id(c))
+# result = add_nums(a,b,c)
+# print(result)
+
+# def test_call_by_value_reference(arg):
+#     print("ID inside the function: " , id(arg))
+#     arg = arg + 1 
+#     print("new object after increment", arg, id(arg))
     
 # greeting = "Hello"
 # print("ID before passing :", id(greeting))
@@ -31,10 +60,10 @@ def test_call_by_value_reference(arg):
 
 
 # Mutable object - list, dictionary, passed by reference
-def testfunction(arg):
-    print("Inside the function: ", arg)
-    print("ID inside the function: ", id(arg))
-    arg = arg.append(100)
+# def testfunction(arg):
+#     print("Inside the function: ", arg)
+#     print("ID inside the function: ", id(arg))
+#     arg = arg.append(100)
 
 # var = [1, 2, 3, 4,5]
 # print("ID before passing: ", id(var))
--- python_basics_learning/basics/tuple_party_trick.py
+++ python_basics_learning/basics/tuple_party_trick.py
@@ -1,5 +1,24 @@
-a = 10 
-b = 20 
+t1 = (1, 1,2, 3, 4)
+
+# print(t1.count(1))
+# print(t1.index(3))
+# for i in t1:
+#     print(i)
+# concatenation - +, *
+# t2 = (5,6)
+# t3 = t2 + t1
+# print(t3)
+# t4 = t2*2
+# print(t4)
+l1 = list(t1)
+print(l1)
+
+
+# t = (1,2,3,4)
+# a, b, c, d = t
+
+# a = 10 
+# b = 20 
 
 
 # Other languages  
@@ -9,16 +28,22 @@ b = 20
 # b = temp
 
 # tuple unpacking 
-a, b = b, a
+# a, b = b, a
 
-print(a, b)
+# print(a, b)
 
-# , makes it tuple 
-t1 = 1,2,3,4
-print(type(t1))
+# # , makes it tuple 
+# t1 = 1,2,3,4
+# print(type(t1))
 
-n = 10,
-print(type(n))
+# n = 10,
+# print(type(n))
+
+# t1 = (2,3,4, [5,6,8, 'e'])
+# # t1[0] = 3
+# print(t1) # TypeError: 'tuple' object does not support item assignment
+# t1[3][3] = 'a'
+# print(t1) 
 
 
 
